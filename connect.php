67a4ec93e91bc8f1c67f05343b207ce145425024
paulopeinado <40861421+paulopeinado@users.noreply.github.com>
2018-07-05 13:27:08 -0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
 // Variaveis Globais
-#$ldap_conn      = getenv($conn);
-$ldap_conn      = "ldaps://172.16.0.52:636";
-#$dominio        = getenv($dom); /*Nome Netbios do dominio*/
-$dominio        = "tj.pa.gov.br"; /*Nome Netbios do dominio*/
+$ldap_server      = getenv("conn");
+#$ldap_conn      = "172.16.0.52";
+$dominio        = getenv("dom"); /*Nome Netbios do dominio*/
+#$dominio        = "tj.pa.gov.br"; /*Nome Netbios do dominio*/
 
 date_default_timezone_set('America/Belem');
 
@@ -12,11 +12,11 @@ $auth_user      = getenv("user");
 $auth_pass      = getenv("pass");
 
 // Base de pesquisa LDAP do Dominio
-#$base_dn = getenv($dn);
-$base_dn = "DC=i,DC=tj,DC=pa,DC=gov,DC=br";
+$base_dn = getenv("dn");
+#base_dn = "DC=i,DC=tj,DC=pa,DC=gov,DC=br";
 
 // Conecta no servidor
-$connect = ldap_connect($ldap_conn) or die( "Could not connect!" );
+$connect = ldap_connect($ldap_server) or die( "Could not connect!" );
 
 // Define que a pesquisa será feita em a partir do diretório raiz do domínio.
 ldap_set_option ($connect, LDAP_OPT_REFERRALS, 0);
