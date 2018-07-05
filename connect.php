1f63faf02d5866fec1d8fd1d3b80a2ca574f7d61
paulopeinado <40861421+paulopeinado@users.noreply.github.com>
2018-07-05 12:31:55 -0300

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,26 @@
+<?php
+// Variaveis Globais
+$ldap_conn      = getenv($conn);
+$dominio        = getenv($dom); /*Nome Netbios do dominio*/
+date_default_timezone_set('America/Belem');
+
+// Usuario para autenticacao na Base LDAP
+$auth_user      = getenv($user);
+$auth_pass      = getenv($pass);
+
+// Base de pesquisa LDAP do Dominio
+$base_dn = getenv($dn);
+
+// Conecta no servidor
+$connect = ldap_connect($ldap_conn) or die( "Could not connect!" );
+
+// Define que a pesquisa será feita em a partir do diretório raiz do domínio.
+ldap_set_option ($connect, LDAP_OPT_REFERRALS, 0);
+
+// Define a versão do protocolo LDAP
+ldap_set_option($connect, LDAP_OPT_PROTOCOL_VERSION, 3) or die ("Could not set ldap protocol");
+
+// Autentica no servidor
+$bind = ldap_bind($connect, $auth_user, $auth_pass) or die ("Could not bind");
+
+?>
