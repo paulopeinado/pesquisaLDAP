53d9e52fe4d22dff7e0783a01ba38ebb335b8b89
paulopeinado <40861421+paulopeinado@users.noreply.github.com>
2018-07-05 13:08:23 -0300

    Add files via upload

--- connect.php
+++ connect.php
@@ -1,7 +1,10 @@
 <?php
 // Variaveis Globais
-$ldap_conn      = getenv($conn);
-$dominio        = getenv($dom); /*Nome Netbios do dominio*/
+#$ldap_conn      = getenv($conn);
+$ldap_conn      = "ldaps://172.16.0.52:636";
+#$dominio        = getenv($dom); /*Nome Netbios do dominio*/
+$dominio        = "tj.pa.gov.br"; /*Nome Netbios do dominio*/
+
 date_default_timezone_set('America/Belem');
 
 // Usuario para autenticacao na Base LDAP
@@ -9,7 +12,8 @@ $auth_user      = getenv($user);
 $auth_pass      = getenv($pass);
 
 // Base de pesquisa LDAP do Dominio
-$base_dn = getenv($dn);
+#$base_dn = getenv($dn);
+$base_dn = "DC=i,DC=tj,DC=pa,DC=gov,DC=br";
 
 // Conecta no servidor
 $connect = ldap_connect($ldap_conn) or die( "Could not connect!" );
